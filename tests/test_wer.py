98ef80c00133f0de22cc2212d92863054cea2559
Galina Kaleeva <galinakaleeva@yandex.ru>
2020-07-07 21:47:02 +0300

    refactor tests for wer and levenshtein

--- tests/test_wer.py
+++ tests/test_wer.py
@@ -7,9 +7,11 @@ from utils.wer import levenshtein_distance, wer
 
 @dataclasses.dataclass
 class Case:
-    first: tp.Sequence[str]
-    second: tp.Sequence[str]
-    result: float
+    first: tp.Sequence[str] = ()
+    second: tp.Sequence[str] = ()
+    recognized: tp.Optional[str] = ''
+    reference: tp.Optional[str] = ''
+    result: float = 0
     name: tp.Optional[str] = None
 
     def __str__(self) -> str:
@@ -30,10 +32,10 @@ TEST_CASES_LEV = [
          'of', 'the', 'algorithm'],
          result=0),
     Case(first=['Levenshtein', 'distance'], second=['equals', 'two'], result=2),
-    Case(first=['there', 'is', 'one', 'insertion'], second=['here', 'there', 'is', 'one', 'insertion'], result=1),
-    Case(first=['here', 'there', 'is', 'also', 'one', 'insertion'],
-         second=['here', 'there', 'is', 'also', 'one', 'little', 'insertion'], result=1),
-    Case(first=['it', 'is', 'deletion', 'here'], second=['it', 'is', 'here'], result=1),
+    Case(first=['there', 'is', 'one', 'deletion'], second=['here', 'there', 'is', 'one', 'deletion'], result=1),
+    Case(first=['here', 'there', 'is', 'also', 'one', 'deletion'],
+         second=['here', 'there', 'is', 'also', 'one', 'little', 'deletion'], result=1),
+    Case(first=['it', 'is', 'insertion', 'here'], second=['it', 'is', 'here'], result=1),
     Case(first=['this', 'is', 'a', 'changed', 'word'], second=['this', 'is', 'a', 'new', 'word'], result=1),
     Case(first=['this', 'is', 'a', 'mixed', 'example'], second=['this', 'example', 'is', 'mixed'], result=3),
     Case(first=['yet', 'another', 'random', 'example'],
@@ -44,23 +46,23 @@ TEST_CASES_LEV = [
 ]
 
 TEST_CASES_WER = [
-    Case(first='', second='a bit of dummy words', result=1),
-    Case(first='word', second='word', result=0),
-    Case(first='long sequence of numerous test words verifying correctness of the algorithm',
-         second='long sequence of numerous test words verifying correctness of the algorithm',
+    Case(recognized='', reference='a bit of dummy words', result=1),
+    Case(recognized='word', reference='word', result=0),
+    Case(recognized='long sequence of numerous test words verifying correctness of the algorithm',
+         reference='long sequence of numerous test words verifying correctness of the algorithm',
          result=0),
-    Case(first='Levenshtein distance', second='equals two', result=1),
-    Case(first='there is one insertion', second='here there is one insertion', result=0.2),
-    Case(first='here there is also one insertion', second='here there is also one little insertion',
+    Case(recognized='Levenshtein distance', reference='equals two', result=1),
+    Case(recognized='there is one deletion', reference='here there is one deletion', result=0.2),
+    Case(recognized='here there is also one deletion', reference='here there is also one little deletion',
          result=0.14285714285714285),
-    Case(first='it is deletion here', second='it is here', result=0.3333333333333333),
-    Case(first='this is a changed word', second='this is a new word', result=0.2),
-    Case(first='this is a mixed example', second='this example is mixed', result=0.75),
-    Case(first='yet another random example', second='just now changed random longer example',
+    Case(recognized='it is insertion here', reference='it is here', result=0.3333333333333333),
+    Case(recognized='this is a changed word', reference='this is a new word', result=0.2),
+    Case(recognized='this is a mixed example', reference='this example is mixed', result=0.75),
+    Case(recognized='yet another random example', reference='just now changed random longer example',
          result=0.6666666666666666),
-    Case(first='пример на русском языке', second='чуточку измененный примерчик не на английском языке',
+    Case(recognized='пример на русском языке', reference='чуточку измененный примерчик не на английском языке',
          result=0.7142857142857143),
-    Case(first='слово ' * BIG_VALUE, second='слово ' * BIG_VALUE, result=0),
+    Case(recognized='слово ' * BIG_VALUE, reference='слово ' * BIG_VALUE, result=0),
 ]
 
 
@@ -72,7 +74,7 @@ def test_lev(t: Case) -> None:
 
 @pytest.mark.parametrize('t', TEST_CASES_WER, ids=str)
 def test_wer(t: Case) -> None:
-    answer = wer(t.first, t.second)
+    answer = wer(t.recognized, t.reference)
     assert answer == t.result
 
 
