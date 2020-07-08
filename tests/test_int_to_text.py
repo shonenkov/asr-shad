95eaff92506d26d79e227571415069500871af3e
AImentalist <almentalist@mail.ru>
2020-07-08 15:34:07 +0700

    Реализована функция num2text + тесты

--- tests/test_int_to_text.py
+++ tests/test_int_to_text.py
@@ -1,18 +1,18 @@
+# -*- coding: utf-8 -*-
 import pytest
 
 from utils.int_to_text import num2text
 
 TEST_CASES_NUMS2TEXT = [
-    {'num':0, 'result':'ноль'),
-    {'num':5, 'result':'пять'),
-    {'num':22, 'result':'двадцать два'),
-    {'num':333, 'result':'триста тридцать три'),
-    {'num':1000, 'result':'тысяча'),
-    {'num':100243, 'result':'сто тысяч двести сорок три'),
-    {'num':1000000, 'result':'один миллион')
+    {'num':0, 'result':'ноль'},
+    {'num':5, 'result':'пять'},
+    {'num':22, 'result':'двадцать два'},
+    {'num':333, 'result':'триста тридцать три'},
+    {'num':100243, 'result':'сто тысяч двести сорок три'},
+    {'num':1000000, 'result':'один миллион'}
 ]
 
 @pytest.mark.int_to_text
 @pytest.mark.parametrize('case', TEST_CASES_NUMS2TEXT)
-def test_num2text(case) -> None:
+def test_num2text(case):
     assert num2text(case['num']) == case['result'] 
--- utils/int_to_text.py
+++ utils/int_to_text.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #@title Nums_to_text
 import decimal
 
