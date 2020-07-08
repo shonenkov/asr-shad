2ef531235adb2f5617ea6b604810ddabee81767c
AImentalist <almentalist@mail.ru>
2020-07-08 15:48:14 +0700

    Добавил тест на отрицательные числа

--- tests/test_int_to_text.py
+++ tests/test_int_to_text.py
@@ -9,7 +9,8 @@ TEST_CASES_NUMS2TEXT = [
     {'num':22, 'result':'двадцать два'},
     {'num':333, 'result':'триста тридцать три'},
     {'num':100243, 'result':'сто тысяч двести сорок три'},
-    {'num':1000000, 'result':'один миллион'}
+    {'num':1000000, 'result':'один миллион'},
+    {'num':-1, 'result':'минус один'}
 ]
 
 @pytest.mark.int_to_text
