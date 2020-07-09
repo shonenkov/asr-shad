6dbe174cd7498010c75b8624bef11757b5fd01b2
Jarosba <YZWL92@gmail.com>
2020-07-09 20:41:42 +0900

    fix pep8 and tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,38 +1,84 @@
 import pytest
+import dataclasses
 import torchaudio
-from utils.SoundDataset import SoundDataset
+from utils.sound_dataset import SoundDataset
+
+FILE_CSV = './tests/test_loader_data/test-example.csv'
+ROOT_DIR = './tests/test_loader_data'
 
 
-file_csv = './tests/test_loader_data/test-example.csv'
-root_dir = './tests/test_loader_data'
+@dataclasses.dataclass
+class Case:
+    marks: dict = None
+    index: int = 0
+
+
+TEST_CASES_LOADER = [
+    Case(index=0,
+         marks={'gender': 'male', 'number': 111, 'sample_rate': 24000,
+                'path': 'test-example/c244c3dc1b.wav'}),
+    Case(index=1,
+         marks={'gender': 'female', 'number': 22, 'sample_rate': 24000,
+                'path': 'test-example/36687b45a7.wav'}),
+    Case(index=2,
+         marks={'gender': 'male', 'number': 33, 'sample_rate': 24000,
+                'path': 'test-example/c1d8ef6242.wav'}),
+    Case(index=3,
+         marks={'gender': 'female', 'number': 44, 'sample_rate': 24000,
+                'path': 'test-example/209c6fb213.wav'})
+]
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER, ids=str)
+def test_gender_loading(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    assert u_set[t.index]['gender'] == t.marks['gender']
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_number_loading(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    assert u_set[t.index]['number'] == t.marks['number']
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_sample_rate_loading(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    assert u_set[t.index]['sample_rate'] == t.marks['sample_rate']
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_path_loading(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    assert u_set[t.index]['path'] == t.marks['path']
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_wavefront_size(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    sample = u_set[t.index]
+    wave, sample_rate = torchaudio.load(ROOT_DIR + '/' + sample['path'])
+    assert sample['waveform'].size() == wave.size()
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_wavefront_loading_1(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    sample = u_set[t.index]
+    wave, sample_rate = torchaudio.load(ROOT_DIR + '/' + sample['path'])
+    idx = wave.size()[1] // 3
+    assert sample['waveform'][0][idx] == wave[0][idx]
+
+
+@pytest.mark.parametrize('t', TEST_CASES_LOADER)
+def test_wavefront_loading_2(t: Case) -> None:
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
+    sample = u_set[t.index]
+    wave, sample_rate = torchaudio.load(ROOT_DIR + '/' + sample['path'])
+    idx = wave.size()[1] // 2
+    assert sample['waveform'][0][idx] == wave[0][idx]
 
 
 def test_loader_len() -> None:
-    u_set = SoundDataset(file_csv, root_dir)
+    u_set = SoundDataset(FILE_CSV, ROOT_DIR)
     assert len(u_set) == 4
-
-
-def test_loader_nextitem() -> None:
-    u_set = SoundDataset(file_csv, './tests/test_loader_data')
-    sample = u_set[2]
-    assert sample['number'] == 33
-
-
-def test_loader_wavefront() -> None:
-    u_set = SoundDataset(file_csv, root_dir)
-    sample = u_set[3]
-    wave, sample_rate = torchaudio.load(root_dir + '/' + sample['path'])
-
-    assert sample['waveform'][0][12] == wave[0][12]
-
-
-def test_loader_gender() -> None:
-    u_set = SoundDataset(file_csv, root_dir)
-    sample = u_set[0]
-    assert sample['gender'] == 'male'
-
-
-def test_loader_rate() -> None:
-    u_set = SoundDataset(file_csv, root_dir)
-    sample = u_set[0]
-    assert sample['sample_rate'] == 24000
--- utils/sound_dataset.py
+++ utils/sound_dataset.py
@@ -1,29 +1,28 @@
 import pandas as pd
 import os
 import torchaudio
-import torch
-import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 
 class SoundDataset(Dataset):
 
     def __init__(self, csv_file, root_dir, transform=None):
         self.root_dir = root_dir
-        self.metrics = pd.read_csv(csv_file)
+        self.marking = pd.read_csv(csv_file)
         self.transform = transform
 
     def __len__(self):
-        return len(self.metrics)
+        return len(self.marking)
 
     def __getitem__(self, idx):
-        path = self.metrics['path'][idx]
-        gender = self.metrics['gender'][idx]
-        number = self.metrics['number'][idx]
+        path = self.marking['path'][idx]
+        gender = self.marking['gender'][idx]
+        number = self.marking['number'][idx]
 
         all_path = os.path.join(self.root_dir, path)
         waveform, sample_rate = torchaudio.load(all_path)
-        sample = {'waveform': waveform, 'gender': gender, 'number': number, 'sample_rate': sample_rate, 'path': path}
+        sample = {'waveform': waveform, 'gender': gender,
+                  'number': number, 'sample_rate': sample_rate, 'path': path}
 
         if self.transform:
             sample = self.transform(sample)
